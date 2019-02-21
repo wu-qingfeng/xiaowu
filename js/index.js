4d974f83380577f60bd9735d8f9c77b6ccbecd3c
wu-qingfeng <1816770409@qq.com>
2019-02-21 12:01:59 +0800

    style Sheet

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 ﻿var msg=["一","二","三","四","五"];
 for(var i=0;i<msg.length;i++){
+    // msg is key
     if(msg[i] == '三'){
         conosle.log(msg[i]);
     }
@@ -10,5 +11,11 @@ if(msg.length >= 3){
 	console.log( ' Hello World' );
 }
 
+for(var r = 0; r < msg.length; r++){
+    // 如果 msg数组等于就赋值为 星期四；
+    if(msg[r] === '四'){
+        msg[r] = '星期四';
+    }
+}
 
 
