9d58d138feca823ae4d7289fa8340ddd7a5897df
wu-qingfeng <1816770409@qq.com>
2019-02-21 11:42:22 +0800

    xingjin

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 ﻿var msg=["一","二","三","四","五"];
 for(var i=0;i<msg.length;i++){
-	conosle.log(msg[i])
+    if(msg[i] == '三'){
+        conosle.log(msg[i]);
+    }
 }
 
-
-if(msg.length === 3){
+/*满足条件就可以执行*/
+if(msg.length >= 3){
 	console.log( ' Hello World' );
 }
+
+
+
